f9579e47ccc9c1ff3934a295da288a9503dfe918
Nicole Parsons <nicoleparsons2930@gmail.com>
2019-02-08 11:57:32 -0500

    Update scene.js

--- Scripts/config/scene.js
+++ Scripts/config/scene.js
@@ -5,6 +5,7 @@ var config;
         Scene[Scene["START"] = 0] = "START";
         Scene[Scene["INGAME"] = 1] = "INGAME";
         Scene[Scene["FINISH"] = 2] = "FINISH";
+        Scene[Scene["PAUSE"] = 3] = "PAUSE";
     })(Scene = config.Scene || (config.Scene = {}));
 })(config || (config = {}));
 //# sourceMappingURL=scene.js.map

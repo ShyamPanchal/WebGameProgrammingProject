019f934fdba9d147a10426ab95c6933c6cf86813
frederico alexandre <fred.bralvalex@gmail.com>
2019-03-15 22:13:49 -0400

    updates for release

--- Scripts/managers/collisions.ts
+++ Scripts/managers/collisions.ts
@@ -1,12 +1,12 @@
 module managers {
     export class Collision {
 
-        public static CheckDistance(obj1: any, obj2: objects.GameObject):boolean {
+        public static CheckDistance(obj1: objects.GameObject, obj2: objects.GameObject):boolean {
             // Create 2 temporary Vec2 objects used for collision detections
             let p1: math.Vec2 = new math.Vec2(obj1.x, obj1.y);
             let p2: math.Vec2 = new math.Vec2(obj2.x, obj2.y);
 
-            if(math.Vec2.Distance(p1, p2) < (obj1.boxCollider.halfH + obj2.boxCollider.halfH)) {
+            if(math.Vec2.Distance(p1, p2) < (obj1.boxCollider.halfH/2 + obj2.boxCollider.halfH/2)) {
                 if(!obj2.isColliding) {
                     // console.log("Colliding with " + obj2.name);
                     switch(obj2.name) {

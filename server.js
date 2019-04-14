f19eb38dbb39ca89156ab49a2d9834a54f0d6530
Shyam <shyampanchal95@gmail.com>
2019-04-13 21:49:02 -0400

    port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(4000, function(){
+connect().use(serveStatic(__dirname)).listen(process.env.PORT || 4000, function(){
     console.log('Server running on 4000...');
 });
